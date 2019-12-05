914291b93ac1a92d366269e9a04500ff3a77bd31
Ahmad Rafiqul A'la <ahmadakla48@gmail.com>
2019-12-06 00:34:30 +0700

    hohooh

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
-const { DATABASE_NAME, DATABASE_HOST_LIVE } = require("./environtment");
+const mongoose = require("mongoose")
+const {DATABASE_HOST, DATABASE_HOST_LIVE_MONGOOSE} = require("./environment")
 
-mongoose
-  .connect(DATABASE_HOST_LIVE, {
+mongoose.connect(DATABASE_HOST_LIVE_MONGOOSE || DATABASE_HOST , {
     useUnifiedTopology: true,
-    useNewUrlParser: true
-  })
-  .then(() => {
-    console.log("Connected to database");
-  })
-  .catch(error => {
-    console.log("Error cant connect to database");
-  });
+    useNewUrlParser : true
+}
 
-const db = mongoose.connection;
-module.exports = db;
+).then(() => {
+    console.log("Connected to database mongoose");
+}).catch(error => {
+    console.log("Error cant connect to database");
+    
+});
+
+const db  = mongoose.connection
+module.exports = db
